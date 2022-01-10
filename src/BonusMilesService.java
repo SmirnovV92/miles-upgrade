4ae253a0f0b1f1799f5a28e337e1f7ea86d2a768
Lorenzo <smirnov_bvb@mail.ru>
2022-01-11 00:18:58 +0300

    Miles upgrade

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price;
+        int amount = 20;
+        int miles = cost / amount;
+
+        return miles;
+    }
+}
